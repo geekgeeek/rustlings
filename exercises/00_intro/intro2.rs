bb1282726a5a69e7483d7318e21e0364b76b3d4e
geekgeeek <geekgomis@gmail.com>
2024-06-04 14:44:50 +0200

    Update intro2.rs

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -8,5 +8,12 @@
 // I AM NOT DONE
 
 fn main() {
-    printline!("Hello there!")
+    println!("Hello there!")
 }
+
+// Pour l'intro1.rs l'erreur dans le code est l'absence de point-virgule à la fin du dernier appel à "println!" dans la fonction main.
+//En Rust, chaque instruction doit se terminer par un point-virgule sauf si elle est suivie par un bloc de code ou
+//si elle est la dernière expression d'une fonction renvoyant une valeur
+
+//Pour l'intro2.rs , l'erreur dans le code se trouve dans l'utilisation incorrecte de la macro printline!. 
+//En Rust, il n'existe pas de macro appelée printline!. La macro correcte pour imprimer une ligne de texte à la console est println!
