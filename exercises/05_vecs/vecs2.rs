b048d7d959338ad414235e39636de6e76c016223
geekgeeek <geekgomis@gmail.com>
2024-06-06 01:11:10 +0200

    Update vecs2.rs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -13,7 +13,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -24,7 +24,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element * 2
     }).collect()
 }
 
@@ -48,3 +48,10 @@ mod tests {
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
+// vecs1.rs :  Le vecteur (Vec) qui contient les mêmes éléments qu'un tableau (array) a. 
+//La macro vec! peut être utilisée pour initialiser le vecteur avec les mêmes éléments que le tableau.
+//Pour corriger ce code, il faut initialiser le vecteur v en utilisant la macro vec! avec les mêmes éléments que le tableau a
+
+// vecs2.rs : Dans la fonction vec_loop, l'erreur est que la valeur actuelle de l'élément dans la boucle n'est pas modifiée. 
+// Pour multiplier chaque élément par 2, vous devez remplacer ??? par *element *= 2. de meme que pour la fonction vec_map
+
