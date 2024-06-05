da0d607e6fbcdcddcdf96044739d89b143ed52f8
geekgeeek <geekgomis@gmail.com>
2024-06-06 00:24:36 +0200

    Update primitive_types5.rs

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -9,7 +9,7 @@
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+   let (name, age) = cat; // Déstructure le tuple
 
     println!("{} is {} years old.", name, age);
 }
