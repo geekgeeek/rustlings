ec33e31f5b27f5fa00047bd3797091e863c3bde1
geekgeeek <geekgomis@gmail.com>
2024-06-06 09:17:42 +0200

    Update tests4.rs

--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -46,3 +46,14 @@ mod tests {
         assert_panics!(Rectangle::new(10, -10));
     }
 }
+
+// tests1.rs : Pour corriger ce test, il faut passer une expression booléenne à la macro assert!
+//Faire que le test compile : Passer une expression booléenne à assert!.
+//Faire que le test passe : Passer une expression qui est toujours vraie.
+//Faire que le test échoue : Passer une expression qui est toujours fausse
+
+// tests3.rs : Le code actuel contient deux tests qui sont incomplets. Il manque les appels à la fonction is_even et les assertions qui vérifient les résultats attendus.
+/*  corection :  Compléter le test pour vérifier si un nombre pair renvoie true.
+Ajouter un test pour vérifier si un nombre impair renvoie false */
+//tests4.rs : dans le test fournis, les assertions ne vérifient pas correctement ces conditions.Correction de la fonction correct_width_and_height
+//Utiliser des macros d'assertion pour les tests avec des valeurs négatives
