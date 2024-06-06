501dbadc7aa8cfccda5c988b7a3fbd18bc2dd407
geekgeeek <geekgomis@gmail.com>
2024-06-06 08:59:32 +0200

    Update errors6.rs

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -100,3 +100,21 @@ mod test {
         assert_eq!(parse_pos_nonzero("42"), Ok(x.unwrap()));
     }
 }
+
+/* errors1.rs : L'erreur dans le code est que la fonction generate_nametag_text retourne actuellement Option<String>,
+mais dans le test explains_why_generating_nametag_text_fails, il est attendu que la fonction retourne un Result<String, String>,
+où le résultat est une erreur avec un message explicatif. Pour corriger cela, il faut modifier la signature de la fonction generate_nametag_text pour qu'elle retourne un Result<String, String> 
+et utiliser Err pour retourner une erreur avec un message explicatif lorsque le nom est une chaîne vide. */
+
+/* errors2.rs : L'erreur dans ce code est que la fonction total_cost retourne toujours Ok quelle que soit la validité de la chaîne fournie. */
+//Pour corriger cela, nous devons utiliser le résultat obtenu après la tentative de conversion de la chaîne en entier (qty). Si la conversion réussit//
+
+/* errors3.rs :L'erreur réside dans l'utilisation de l'opérateur ? dans la fonction main(). L'opérateur ? est utilisé pour propager les erreurs,
+//Pour résoudre cela, nous devons utiliser une gestion manuelle des erreurs dans la fonction main(). Cela signifie que nous devons extraire manuellement le résultat de total_cost et traiter les erreurs à l'aide d'un bloc match ou d'une méthode comme unwrap()
+
+//errors4.rs : 
+//L'erreur dans cette méthode se situe dans la conversion de value en u64. Même si value est négatif, la conversion en u64 produit toujours une valeur positiv.
+//Pour corriger cela, nous devons ajouter des vérifications pour détecter les valeurs négatives et les valeurs nulles
+
+//errors5.rs : Le principal problème à résoudre dans ce code est de choisir le trait approprié pour les erreurs possibles que peut rencontrer la fonction main(). Dans ce cas, les erreurs possibles sont de type CreationError et ParseIntError.
+/* errors6.rs:L'erreur dans ce code est que la fonction de conversion d'erreur pour ParseIntError n'est pas implémentée. 
