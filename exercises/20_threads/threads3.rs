97852304ec5d9d6488baa3ef282bb0a63c36ba06
geekgeeek <geekgomis@gmail.com>
2024-06-06 10:04:56 +0200

    Update threads3.rs

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -23,6 +23,8 @@ impl Queue {
             first_half: vec![1, 2, 3, 4, 5],
             second_half: vec![6, 7, 8, 9, 10],
         }
+
+        //
     }
 }
 
@@ -63,3 +65,17 @@ fn main() {
     println!("total numbers received: {}", total_received);
     assert_eq!(total_received, queue_length)
 }
+
+/* threads1.rs L'erreur se trouve dans la boucle for où les résultats sont collectés dans le vecteur results. 
+Pour corriger cela, vous pouvez utiliser la méthode join pour récupérer les valeurs renvoyées par chaque thread.*/
+
+/* threads2.rs : L'erreur dans le code se trouve dans la tentative de mise à jour de la valeur partagée jobs_completed dans chaque thread 
+sans prendre de précautions pour la synchronisation des accès concurrents à cette valeur.
+Pour corriger cela, vous devez utiliser un mécanisme de synchronisation tel que Mutex ou Atomic
+pour garantir que les threads accèdent à jobs_completed de manière sûre et sans aucune course.*/
+
+/* threads3.rs :Dans ce code, nous avons un problème de propriété (ownership) lorsque nous passons la structure Queue à la fonction send_tx. 
+La fonction send_tx reçoit la structure Queue par valeur (q: Queue), Pour résoudre ce problème, nous devons modifier la signature de la fonction send_tx pour qu'elle accepte une référence à Queue plutôt qu'une valeur./
+
+
+
