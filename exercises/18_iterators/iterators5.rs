06972bbde8046f1e2aae1762fc693c0022147af5
geekgeeek <geekgomis@gmail.com>
2024-06-06 06:28:22 +0200

    Update iterators5.rs

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -21,6 +21,14 @@ enum Progress {
     Some,
     Complete,
 }
+fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    map.values().filter(|&v| *v == value).count()
+}
+
+fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    collection.iter().map(|map| count_iterator(map, value)).sum()
+}
+
 
 fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
     let mut count = 0;
