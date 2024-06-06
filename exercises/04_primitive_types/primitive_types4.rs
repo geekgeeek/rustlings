30fdd04493ade16dd65ea39628615c711c27d356
geekgeeek <geekgomis@gmail.com>
2024-06-06 11:36:54 +0200

    Update primitive_types4.rs

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -13,5 +13,10 @@ fn slice_out_of_array() {
 
     let nice_slice = &a[1..4]; // Crée une tranche contenant les éléments 2, 3, 4
 
-    assert_eq!([2, 3, 4], nice_slice)
+    // Compare la tranche nice_slice avec une tranche littérale [2, 3, 4]
+    assert_eq!(nice_slice, &[2, 3, 4]);
+}
+
+fn main() {
+    slice_out_of_array();
 }
