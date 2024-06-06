0b2d0b67776d6c4cce517b5a09cefdfa564f9f5e
geekgeeek <geekgomis@gmail.com>
2024-06-06 04:30:27 +0200

    Update move_semantics2.rs

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -17,9 +17,7 @@ fn main() {
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
