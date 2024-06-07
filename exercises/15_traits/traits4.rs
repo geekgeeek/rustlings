09e67aa493f42906a9a6befa584789d4707d4451
geekgeeek <geekgomis@gmail.com>
2024-06-07 09:53:59 +0200

    Update traits4.rs

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -36,7 +36,7 @@ mod tests {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
 
-        assert!(compare_license_types(some_software, other_software));
+        assert!(compare_license_types(&some_software, &other_software));
     }
 
     #[test]
@@ -44,6 +44,6 @@ mod tests {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
 
-        assert!(compare_license_types(other_software, some_software));
+        assert!(compare_license_types(&other_software, &some_software));
     }
 }
